486cb73a2a649a57d730df18ec18294d10cd1a2c
weijianteoh2000 <91655051+weijianteoh2000@users.noreply.github.com>
2022-10-04 00:42:08 +0800

    Add files via upload

---
+++ views/PlayerViews.js
@@ -0,0 +1,147 @@
+import React from 'react';
+
+const exports = {};
+
+// Player views must be extended.
+// It does not have its own Wrapper view.
+
+exports.GuestHand = class extends React.Component {
+  render() {
+    const {parent, playable, guest} = this.props;
+    return (
+      <div>
+        {guest? 'It was a draw! Pick again.' : ''}
+        <br />
+        {playable ? 'Please guest the total number' : ''}
+        <br />
+        <button
+          disabled={!playable}
+          onClick={() => parent.playGuest(0)}
+        >0</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playGuest(1)}
+        >1</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playGuest(2)}
+        >2</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playGuest(3)}
+        >3</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playGuest(4)}
+        >4</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playGuest(5)}
+        >5</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playGuest(6)}
+        >6</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playGuest(7)}
+        >7</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playGuest(8)}
+        >8</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playGuest(9)}
+        >9</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playGuest(10)}
+        >10</button>
+      </div>
+    );
+  }
+}
+
+exports.GetHand = class extends React.Component {
+  render() {
+    const {parent, playable, hand} = this.props;
+    return (
+      <div>
+        {hand ? 'It was a draw! Pick again.' : ''}
+        <br />
+        {playable ? 'Please choose your number' : ''}
+        <br />
+        <button
+          disabled={!playable}
+          onClick={() => parent.playHand(0)}
+        >0</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playHand(1)}
+        >1</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playHand(2)}
+        >2</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playHand(3)}
+        >3</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playHand(4)}
+        >4</button>
+        <button
+          disabled={!playable}
+          onClick={() => parent.playHand(5)}
+        >5</button>
+      </div>
+    );
+  }
+}
+
+exports.WaitingForResults = class extends React.Component {
+  render() {
+    return (
+      <div>
+        Waiting for results...
+      </div>
+    );
+  }
+}
+
+exports.DoneForPlayNum = class extends React.Component {
+  render() {
+    const {hand} = this.props;
+    return (
+      <div>
+        You have choose {hand} 
+      </div>
+    );
+  }
+}
+
+exports.Done = class extends React.Component {
+  render() {
+    const {outcome} = this.props;
+    return (
+      <div>
+        Thank you for playing. The outcome of this game was:
+        <br />{outcome || 'Unknown'}
+      </div>
+    );
+  }
+}
+
+exports.Timeout = class extends React.Component {
+  render() {
+    return (
+      <div>
+        There's been a timeout. (Someone took too long.)
+      </div>
+    );
+  }
+}
+
+export default exports;
